d2a03423b7fc02b9206df03ed371cf3bd98fb112
Jagannath Pandit <35166514+99jagannath@users.noreply.github.com>
2019-06-08 23:38:50 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,40 +1,29 @@
-// enable offline data
-db.enablePersistence()
-  .catch(function(err) {
-    if (err.code == 'failed-precondition') {
-      // probably multible tabs open at once
-      console.log('persistance failed');
-    } else if (err.code == 'unimplemented') {
-      // lack of browser support for the feature
-      console.log('persistance not available');
-    }
-  });
+const recipes = document.querySelector('.recipes');
 
-// real-time listener
-db.collection('recipes').onSnapshot(snapshot => {
-  snapshot.docChanges().forEach(change => {
-    if(change.type === 'added'){
-      renderRecipe(change.doc.data(), change.doc.id);
-    }
-    if(change.type === 'removed'){
-      // remove the document data from the web page
-    }
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right'});
+  // add recipe form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'left'});
 });
 
-// add new recipe
-const form = document.querySelector('form');
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  
-  const recipe = {
-    name: form.title.value,
-    ingredients: form.ingredients.value
-  };
+// render recipe data
+const renderRecipe = (data, id) => {
 
-  db.collection('recipes').add(recipe)
-    .catch(err => console.log(err));
+  const html = `
+    <div class="card-panel recipe white row" data-id="${id}">
+      <img src="/img/dish.png" alt="recipe thumb">
+      <div class="recipe-details">
+        <div class="recipe-title">${data.name}</div>
+        <div class="recipe-ingredients">${data.ingredients}</div>
+      </div>
+      <div class="recipe-delete">
+        <i class="material-icons" data-id="${id}">delete_outline</i>
+      </div>
+    </div>
+  `;
+  recipes.innerHTML += html;
 
-  form.title.value = '';
-  form.ingredients.value = '';
-});
+};
